a8c9e0ce61fc82f472a8a11ca901755dd2c5ead8
Mateo Luis Valdesolo <81998104+mateoValdesolo@users.noreply.github.com>
2022-09-14 19:44:28 -0300

    Cambios en Main

--- Concurrente2022/src/tp3/ejercicio5/Main.java
+++ Concurrente2022/src/tp3/ejercicio5/Main.java
@@ -6,19 +6,19 @@ package tp3.ejercicio5;
  */
 public class Main {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		Surtidor surtidor = new Surtidor();
-		Thread autos[] = new Thread[10];
+        Surtidor surtidor = new Surtidor();
+        Thread autos[] = new Thread[10];
 
-		for (int i = 0; i < autos.length; i++) {
-			autos[i] = new Thread(new Auto("AC897GT", "Subaru", "Impreza", 500, surtidor), "Auto #" + i);
-		}
+        for (int i = 0; i < autos.length; i++) {
+            autos[i] = new Thread(new Auto("AC897GT", "Subaru", "Impreza", 500, surtidor, 60, 6), "Auto #" + i);
+        }
 
-		for (int i = 0; i < autos.length; i++) {
-			autos[i].start();
-		}
+        for (int i = 0; i < autos.length; i++) {
+            autos[i].start();
+        }
 
-	}
+    }
 
 }
