3f67f8f0cbda27be161e4458348bee9be4de61f0
Mateo Luis Valdesolo <81998104+mateoValdesolo@users.noreply.github.com>
2022-09-14 19:00:09 -0300

    Cambio en Main

--- Concurrente2022/src/tp3/ejercicio7/Main.java
+++ Concurrente2022/src/tp3/ejercicio7/Main.java
@@ -9,13 +9,14 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Letra letra = new Letra();
-		Thread hiloA = new Thread(new Impresora('A', letra));
-		Thread hiloB = new Thread(new Impresora('B', letra));
-		Thread hiloC = new Thread(new Impresora('C', letra));
-		
-		hiloA.start();
-		hiloB.start();
-		hiloC.start();
+        	Impresora.setRonda(4);
+        	Thread hiloA = new Thread(new Impresora('A', letra, 1));
+        	Thread hiloB = new Thread(new Impresora('B', letra, 2));
+        	Thread hiloC = new Thread(new Impresora('C', letra, 3));
+
+        	hiloA.start();
+        	hiloB.start();
+        	hiloC.start();
 	}
 
 }
