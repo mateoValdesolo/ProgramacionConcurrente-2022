12ec1cf21fc417487350f1638400b65b3b462156
Mateo Luis Valdesolo <81998104+mateoValdesolo@users.noreply.github.com>
2022-09-14 19:46:04 -0300

    Cambios en Surtidor

--- Concurrente2022/src/tp3/ejercicio5/Surtidor.java
+++ Concurrente2022/src/tp3/ejercicio5/Surtidor.java
@@ -6,37 +6,33 @@ package tp3.ejercicio5;
  */
 public class Surtidor {
 
-	private int maximo;
+    private int maximo;
 
-	public Surtidor() {
-		this.maximo = 15000;
-	}
+    public Surtidor() {
+        this.maximo = 15000;
+    }
 
-	public void cargarCombustible(int lts) {
-		synchronized (this) {
-			if ((maximo - lts) >= 0) {
-				System.out.println(Thread.currentThread().getName() + " Va a cargar combustible");
-				this.maximo -= lts;
-				espera();
-				System.out.println(Thread.currentThread().getName()
-						+ " Termino de cargar combustible, combustible en surtidor: " + maximo);
-			}
-		
-		}
-	}
+    public synchronized void cargarCombustible(int lts) {
+        if ((maximo - lts) >= 0) {
+            System.out.println(Thread.currentThread().getName() + " Va a cargar combustible");
+            this.maximo -= lts;
+            espera();
+            System.out.println(Thread.currentThread().getName()
+                    + " Termino de cargar combustible, combustible en surtidor: " + maximo);
+        }
+        // Puedo implemetar que cargue lo que queda, sea la cantidad que tenga que cargar
+    }
 
-	public synchronized int getMaximo() {
-		synchronized (this) {
-			return maximo;
-		}
-	}
-	
-	private void espera() {
-		try {
-			Thread.sleep(4000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
+    public synchronized int getMaximo() {
+        return maximo;
+    }
+
+    private void espera() {
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
